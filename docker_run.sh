31f3962cc8f91380fe4bc4f855a4e79f65b036b9
Jonathan Martins <web@jonathanmartins.com.br>
2022-04-05 02:37:39 -0300

    config

--- docker_run.sh
+++ docker_run.sh
@@ -9,8 +9,8 @@ PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
-    -p 80:80 \
-    -e TZ="America/Chicago" \
+    -p 8053:80 \
+    -e TZ="America/Sao_Paulo" \
     -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
@@ -18,7 +18,7 @@ docker run -d \
     --hostname pi.hole \
     -e VIRTUAL_HOST="pi.hole" \
     -e PROXY_LOCATION="pi.hole" \
-    -e ServerIP="127.0.0.1" \
+    -e ServerIP="192.168.1.5" \
     pihole/pihole:latest
 
 printf 'Starting up pihole container '
